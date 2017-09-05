85bebc21602c2fd56f555c2c66d75c68b9a3b3d7
aivano <antun.ivanovic@fer.hr>
2017-09-05 19:01:45 +0200

    Fixed some issues in arducopter bridge.

--- mmuav_arducopter_bridge/src/RCIn2OverrideRCin.py
+++ mmuav_arducopter_bridge/src/RCIn2OverrideRCin.py
@@ -3,29 +3,72 @@
 import rospy
 from mavros_msgs.msg import RCIn
 from mavros_msgs.msg import OverrideRCIn
+from mav_msgs.msg import Actuators
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import Float64MultiArray, Float64
 
 
 class RCIn2OverrideRCIn():
-	def __init__(self):
+    def __init__(self):
 
-		rospy.Subscriber('/jeti/mavros/rc/in', RCIn, self.rcin_callback)
-		self.overrideRCIn_pub = rospy.Publisher('/arducopter/mavros/rc/override', OverrideRCIn, queue_size=10)
-		self.overrideRCIn_msg = OverrideRCIn()
+        self.overrideRCIn_pub = rospy.Publisher('mavros/rc/override', OverrideRCIn, queue_size=1)
+        self.received_rc_msg = OverrideRCIn()
 
-	def run(self):
+        self.euler_ref_pub = rospy.Publisher('euler_ref', Vector3, queue_size=1)
+        self.euler_ref_msg = Vector3()
 
-		rospy.spin()
+        self.vpc_roll = 0.0
+        self.vpc_pitch = 0.0
+        self.motor_velocity = 0.0
 
+        self.rate = rospy.get_param("~rate", 50)
 
-	def rcin_callback(self,msg):
-		#self.overrideRCIn_msg.channels.clear()
-		#for i in range(8):
-		if len(msg.channels) >=8:
-			self.overrideRCIn_msg.channels = msg.channels[0:8]	
-			self.overrideRCIn_pub.publish(self.overrideRCIn_msg)
+        rospy.Subscriber('/jeti/mavros/rc/in', RCIn, self.rcin_callback)
+        rospy.Subscriber('attitude_command', Float64MultiArray, 
+            self.attitude_command_cb, queue_size=1)
+        rospy.Subscriber('mot_vel_ref', Float64, self.mot_vel_ref_cb)
+
+    def run(self):
+        
+        while not rospy.is_shutdown():
+            rospy.sleep(1.0/float(self.rate))
+            mode = self.check_mode()
+
+            if mode == 0:
+                # attitude only, overriding message
+                rc_channels = OverrideRCIn()
+                for i in range(8):
+                    rc_channels.channels[i] = self.received_rc_msg.channels[i]
+
+                rc_channels.channels[1] = 1500 + 2.5*self.vpc_roll
+                rc_channels.channels[0] = 1500 + 2.5*self.vpc_pitch
+                self.overrideRCIn_pub.publish(rc_channels)
+
+    def check_mode(self):
+        return 0
+
+    def rcin_callback(self,msg):
+        #self.overrideRCIn_msg.channels.clear()
+        #for i in range(8):
+        if len(msg.channels) >=8:
+            self.received_rc_msg = msg
+
+        self.euler_ref_msg.x = (self.received_rc_msg.channels[1] - 1500)*0.15/500.0
+        self.euler_ref_msg.y = (self.received_rc_msg.channels[0] - 1500)*0.15/500.0
+        self.euler_ref_pub.publish(self.euler_ref_msg)
+
+    def attitude_command_cb(self,msg):
+        if len(msg.data) < 5:
+            print "Not enough data, should be 5. Length: ", len(msg.data)
+        else:
+            self.vpc_roll = msg.data[3]
+            self.vpc_pitch = msg.data[4]
+
+    def mot_vel_ref_cb(self,msg):
+        self.motor_velocity = 0.0
 
 if __name__ == '__main__':
 
-	rospy.init_node('RCIn_to_OverrideRCIn')
-	conversion = RCIn2OverrideRCIn()
-	conversion.run()
+    rospy.init_node('RCIn_to_OverrideRCIn')
+    conversion = RCIn2OverrideRCIn()
+    conversion.run()
--- mmuav_arducopter_bridge/src/gazebo_to_arducopter.py
+++ mmuav_arducopter_bridge/src/gazebo_to_arducopter.py
@@ -7,89 +7,88 @@ from struct import *
 import serial
 import time
 
-
 class StepperInterface():
-	def __init__(self):
+    def __init__(self):
 
-		##Setup serial port 
-		self.serial_device = serial.Serial()
+        ##Setup serial port 
+        self.serial_device = serial.Serial()
 
-		self.serial_device.port = '/dev/ttyUSB0'
-		self.serial_device.baudrate = 115200
-		self.serial_device.parity = serial.PARITY_NONE
-		self.serial_device.stopbits = serial.STOPBITS_ONE
-		self.serial_device.bytesize = serial.EIGHTBITS
+        self.serial_device.port = '/dev/ttyUSB0'
+        self.serial_device.baudrate = 115200
+        self.serial_device.parity = serial.PARITY_NONE
+        self.serial_device.stopbits = serial.STOPBITS_ONE
+        self.serial_device.bytesize = serial.EIGHTBITS
 
-		self.mass1 = 0
-		self.mass2 = 0
-		self.mass3 = 0
-		self.mass4 = 0
+        self.mass1 = 0
+        self.mass2 = 0
+        self.mass3 = 0
+        self.mass4 = 0
 
-		self.r = rospy.Rate(60)
+        self.r = rospy.Rate(60)
 
-		rospy.Subscriber('/arducopter/movable_mass_0_position_controller/command', Float64, self.mm1)
-		rospy.Subscriber('/arducopter/movable_mass_1_position_controller/command', Float64, self.mm2)
-		rospy.Subscriber('/arducopter/movable_mass_2_position_controller/command', Float64, self.mm3)
-		rospy.Subscriber('/arducopter/movable_mass_3_position_controller/command', Float64, self.mm4)
+        rospy.Subscriber('movable_mass_0_position_controller/command', Float64, self.mm1_cb)
+        rospy.Subscriber('movable_mass_1_position_controller/command', Float64, self.mm2_cb)
+        rospy.Subscriber('movable_mass_2_position_controller/command', Float64, self.mm3_cb)
+        rospy.Subscriber('movable_mass_3_position_controller/command', Float64, self.mm4_cb)
 
-	def run(self):
+    def run(self):
 
-		##Open communication
-		print 'Opening serial communication'
-		self.serial_device.open()
+        ##Open communication
+        print 'Opening serial communication'
+        self.serial_device.open()
 
-		while not self.serial_device.is_open:
-			print 'Waiting...'
+        while not self.serial_device.is_open:
+            print 'Waiting...'
 
-		print 'Serial communication opened!'
+        print 'Serial communication opened!'
 
-		while not rospy.is_shutdown():
-			##Check for messages with 60 Hz
+        while not rospy.is_shutdown():
+            ##Check for messages with 60 Hz
 
-			s = pack('iiiicxxx', self.mass1, self.mass2, self.mass3, self.mass4, 'C')
+            s = pack('iiiicxxx', self.mass1, self.mass2, self.mass3, self.mass4, 'C')
 
-			##Debug print
-			print self.mass1
+            ##Debug print
+            print self.mass1
 
-			self.serial_device.write(s)
-			self.r.sleep()
+            self.serial_device.write(s)
+            self.r.sleep()
 
 
-	def mm1(self,msg):
-		if (msg.data > 0.08):
-			msg.data = 0.08
-		elif (msg.data < -0.08):
-			msg.data = -0.08
+    def mm1_cb(self,msg):
+        if (msg.data > 0.08):
+            msg.data = 0.08
+        elif (msg.data < -0.08):
+            msg.data = -0.08
 
-		self.mass1 = round(5000*msg.data)
+        self.mass1 = round(5000*msg.data)
 
-	def mm2(self,msg):
-		if (msg.data > 0.08):
-			msg.data = 0.08
-		elif (msg.data < -0.08):
-			msg.data = -0.08
+    def mm2_cb(self,msg):
+        if (msg.data > 0.08):
+            msg.data = 0.08
+        elif (msg.data < -0.08):
+            msg.data = -0.08
 
-		self.mass2 = round(5000*msg.data)
+        self.mass2 = round(5000*msg.data)
 
-	def mm3(self,msg):
-		if (msg.data > 0.08):
-			msg.data = 0.08
-		elif (msg.data < -0.08):
-			msg.data = -0.08
+    def mm3_cb(self,msg):
+        if (msg.data > 0.08):
+            msg.data = 0.08
+        elif (msg.data < -0.08):
+            msg.data = -0.08
 
-		self.mass3 = round(5000*msg.data)
+        self.mass3 = round(5000*msg.data)
 
-	def mm4(self,msg):
-		if (msg.data > 0.08):
-			msg.data = 0.08
-		elif (msg.data < -0.08):
-			msg.data = -0.08
+    def mm4_cb(self,msg):
+        if (msg.data > 0.08):
+            msg.data = 0.08
+        elif (msg.data < -0.08):
+            msg.data = -0.08
 
-		self.mass4 = round(5000*msg.data)
+        self.mass4 = round(5000*msg.data)
 
 
 if __name__ == '__main__':
 
-	rospy.init_node('Serial_communication')
-	sercom = StepperInterface()
-	sercom.run()
+    rospy.init_node('Serial_communication')
+    sercom = StepperInterface()
+    sercom.run()
