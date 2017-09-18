20ff3f70046d38da8af03f2f795ff7a67cd47274
aivano <antun.ivanovic@fer.hr>
2017-09-18 10:10:32 +0200

    Added joystick control

--- mmuav_control/src/mmuav_controller_outputs_to_motor_velocities.py
+++ mmuav_control/src/mmuav_controller_outputs_to_motor_velocities.py
@@ -11,6 +11,7 @@ import math
 from mav_msgs.msg import Actuators
 from datetime import datetime
 from rosgraph_msgs.msg import Clock
+import time
 
 class MergeControllerOutputs:
 
@@ -27,25 +28,39 @@ class MergeControllerOutputs:
         self.mot_vel_ref = 0.0
 
         self.attitude_command_received_flag = False
-        self.mot_vel_ref_received_flag = True
-        self.q1_left = -1.0+1.57
-        self.q2_left = -2.0+1.57
-        self.q1_right = 2.0-1.57
-        self.q2_right = -2.0+1.57
+        self.mot_vel_ref_received_flag = False
+        self.q1_left = 2.493
+        self.q2_left = -2.56
+        self.q3_left = -1.08
+        self.q1_right = -0.645
+        self.q2_right = -2.567
+        self.q3_right = -1.08
 
         # Publisher for motor velocities
         self.mot_vel_pub = rospy.Publisher('/gazebo/command/motor_speed', 
             Actuators, queue_size=1)
         self.q1_left_pub = rospy.Publisher('joint1_left_controller/command', Float64, queue_size=1)
         self.q2_left_pub = rospy.Publisher('joint2_left_controller/command', Float64, queue_size=1)
+        self.q3_left_pub = rospy.Publisher('joint3_left_controller/command', Float64, queue_size=1)
         self.q1_right_pub = rospy.Publisher('joint1_right_controller/command', Float64, queue_size=1)
         self.q2_right_pub = rospy.Publisher('joint2_right_controller/command', Float64, queue_size=1)
+        self.q3_right_pub = rospy.Publisher('joint3_right_controller/command', Float64, queue_size=1)
 
         # Subscribers to height and attitude controllers
         rospy.Subscriber('attitude_command', Float64MultiArray, 
             self.attitude_command_cb, queue_size=1)
         rospy.Subscriber('mot_vel_ref', Float64, 
             self.motor_velocity_ref_cb, queue_size=1)
+
+        # Sleep for few seconds then publish initial arms q
+        rospy.sleep(2.0)
+        self.q1_left_pub.publish(Float64(self.q1_left))
+        self.q2_left_pub.publish(Float64(self.q2_left))
+        self.q3_left_pub.publish(Float64(self.q3_left))
+        self.q1_right_pub.publish(Float64(self.q1_right))
+        self.q2_right_pub.publish(Float64(self.q2_right))
+        self.q3_right_pub.publish(Float64(self.q3_right))
+
 
     def run(self):
         while (not self.attitude_command_received_flag) and (not rospy.is_shutdown()):
@@ -74,8 +89,10 @@ class MergeControllerOutputs:
 
             self.q1_left_pub.publish(Float64(self.q1_left))
             self.q2_left_pub.publish(Float64(self.q2_left))
+            self.q3_left_pub.publish(Float64(self.q3_left))
             self.q1_right_pub.publish(Float64(self.q1_right))
             self.q2_right_pub.publish(Float64(self.q2_right))
+            self.q3_right_pub.publish(Float64(self.q3_right))
 
 
     def attitude_command_cb(self, msg):
@@ -85,8 +102,10 @@ class MergeControllerOutputs:
             self.attitude_command_received_flag = True
             self.q1_left = msg.data[0]
             self.q2_left = msg.data[1]
+            self.q3_left = -msg.data[2]
             self.q1_right = msg.data[3]
             self.q2_right = msg.data[4]
+            self.q3_right = -msg.data[5]
             self.yaw_command = msg.data[6]
             self.vpc_roll_command = msg.data[7]
             self.vpc_pitch_command = msg.data[8]
