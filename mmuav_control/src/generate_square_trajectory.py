94fd2a8f7b0045878e1d6e0a1a3f27d778470888
aivano <antun.ivanovic@fer.hr>
2017-11-26 18:21:26 +0100

    Added trajectory generation and processing files to publish as reference for uav.

---
+++ mmuav_control/src/generate_square_trajectory.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import sys, os
+import rospy
+import copy
+import math
+import time
+
+from geometry_msgs.msg import Pose, Point, Quaternion
+from mav_path_trajectory.msg import WaypointArray
+from mav_path_trajectory.srv import WaypointArraySrv
+
+class SquareTrajectory():
+
+    def __init__(self):
+        self.waypointArray = WaypointArray()
+        # Services for requesting trajectory interpolation
+        rospy.wait_for_service("positionWaypointArray", timeout=30)
+        self.waypointArrayService = rospy.ServiceProxy("positionWaypointArray", 
+            WaypointArraySrv)
+
+        self.tempPose = Pose()
+        self.tempPose.orientation = copy.deepcopy(Quaternion(0,0,0,1))
+
+        n = 5
+        for i in range(0,n):
+            self.waypointArray.waypoints.append(copy.deepcopy(self.tempPose))
+
+        
+        self.waypointArray.waypoints[0].position = copy.deepcopy(Point(0.0,0.0,1))
+        self.waypointArray.waypoints[1].position = copy.deepcopy(Point(1.0,0.0,1))
+        self.waypointArray.waypoints[2].position = copy.deepcopy(Point(1.0,1.0,1))
+        self.waypointArray.waypoints[3].position = copy.deepcopy(Point(0.0,1.0,1))
+        self.waypointArray.waypoints[4].position = copy.deepcopy(Point(0.0,0.0,1))
+        #self.waypointArray.waypoints[5].position = copy.deepcopy(Point(0,-0.3,1))
+        #self.waypointArray.waypoints[6].position = copy.deepcopy(Point(-0.6,-0.3,1))
+        #self.waypointArray.waypoints[7].position = copy.deepcopy(Point(-0.6,-0.9,1))
+        #self.waypointArray.waypoints[8].position = copy.deepcopy(Point(-0.6,-1.5,1))
+        
+        """
+        self.waypointArray.waypoints[0].position = copy.deepcopy(Point(0,0,1))
+        self.waypointArray.waypoints[1].position = copy.deepcopy(Point(0.25,0,1))
+        self.waypointArray.waypoints[2].position = copy.deepcopy(Point(0.5,0,1))
+        self.waypointArray.waypoints[3].position = copy.deepcopy(Point(0.5,1.5,1))
+        self.waypointArray.waypoints[4].position = copy.deepcopy(Point(0.5,3,1))
+        self.waypointArray.waypoints[5].position = copy.deepcopy(Point(0.25,3,1))
+        self.waypointArray.waypoints[6].position = copy.deepcopy(Point(0,3,1))
+        self.waypointArray.waypoints[7].position = copy.deepcopy(Point(0,1.5,1))
+        self.waypointArray.waypoints[8].position = copy.deepcopy(Point(0,0,1))
+        """
+        """
+        self.waypointArray.waypoints[0].position = copy.deepcopy(Point(0.5,0,1))
+        self.waypointArray.waypoints[1].position = copy.deepcopy(Point(0.5,1.5,1))
+        self.waypointArray.waypoints[2].position = copy.deepcopy(Point(1,3,1))
+        self.waypointArray.waypoints[3].position = copy.deepcopy(Point(1,1.5,1))
+        """
+        self.waypointArrayService(self.waypointArray)
+        #print self.waypointArray
+        print "Service called"
+
+
+if __name__=="__main__":
+    rospy.init_node("SquareTrajectory")
+    SquareTrajectory()
+
+
---
+++ mmuav_control/src/trajectory_to_position_reference.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys, os
+import rospy
+import copy
+import math
+import time
+
+from trajectory_msgs.msg import MultiDOFJointTrajectory
+from geometry_msgs.msg import Vector3
+
+
+class TrajectoryToPositionReference():
+
+    def __init__(self):
+        rospy.Subscriber('/euroc3/command/trajectory', MultiDOFJointTrajectory, 
+            self.trajectory_callback, queue_size=1)
+        self.position_ref_pub = rospy.Publisher('pos_ref', Vector3, queue_size=1)
+
+    def run(self):
+        rospy.spin()
+
+    def trajectory_callback(self, msg):
+        trajectory = msg
+
+        print "Trajectory received"
+        rate = rospy.Rate(100)
+        position_ref = Vector3()
+        for i in range(len(trajectory.points)):
+            rate.sleep()
+            position_ref.x = trajectory.points[i].transforms[0].translation.x
+            position_ref.y = trajectory.points[i].transforms[0].translation.y
+            position_ref.z = trajectory.points[i].transforms[0].translation.z
+            self.position_ref_pub.publish(position_ref)
+
+if __name__=="__main__":
+    rospy.init_node("TrajectoryToPositionReference")
+    traj = TrajectoryToPositionReference()
+    traj.run()
