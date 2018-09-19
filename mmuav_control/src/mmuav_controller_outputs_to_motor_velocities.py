5229931c154aa13c7d1e46d58a8ca11eb310f00d
Lovro Markovic <lovro.markovic@fer.hr>
2018-09-19 14:57:45 +0200

    .

--- mmuav_control/src/mmuav_controller_outputs_to_motor_velocities.py
+++ mmuav_control/src/mmuav_controller_outputs_to_motor_velocities.py
@@ -20,7 +20,7 @@ from control_msgs.msg import JointControllerState
 class MergeControllerOutputs:
 
     def __init__(self):
-        self.rate = rospy.get_param('rate', 50)
+        self.rate = rospy.get_param('rate', 1)
         self.ros_rate = rospy.Rate(self.rate)
 
         self.roll_command = 0.0
@@ -58,11 +58,11 @@ class MergeControllerOutputs:
         self.dx = 0
         self.dy = 0
 
-        self.x_right_offset = -0.23499987352
-        self.y_right_offset = 0.000136305972549
+        self.x_right_offset = 0.149950633999
+        self.y_right_offset = -0.00240184654697
 
-        self.x_left_offset = -0.234999660588
-        self.y_left_offset = 0.000345747003094
+        self.x_left_offset = 0.150487144757
+        self.y_left_offset = -0.00204565739011
 
         self.L1 = 0.094
         self.L2 = 0.061
@@ -72,20 +72,27 @@ class MergeControllerOutputs:
 
          # Sleep for few seconds then publish initial arms q
         print("Setting initial pos")
-        rospy.sleep(5)
+        rospy.sleep(5.0)
         self.q1_left_pub.publish(Float64(2.493))
         self.q2_left_pub.publish(Float64(-2.56))
         self.q3_left_pub.publish(Float64(-1.08))
         self.q1_right_pub.publish(Float64(-0.645))
         self.q2_right_pub.publish(Float64(-2.567))
         self.q3_right_pub.publish(Float64(-1.08))
-        rospy.sleep(5.0)
+        rospy.sleep(10)
 
         while not rospy.is_shutdown():
             self.ros_rate.sleep()
             
-            self.get_new_dqR(self.dx, self.dy, 0.15)
-            self.get_new_dqL(self.dx, self.dy, 0.15)
+            dqR = self.get_new_dqR(self.dx, self.dy, 0.01)
+            dqL = self.get_new_dqL(self.dx, self.dy, 0.01)
+
+            self.q1_left_pub.publish(Float64(self.q1_left + dqL[0] + 1.57))
+            self.q2_left_pub.publish(Float64(self.q2_left + dqL[1] - 1.57))
+            self.q3_left_pub.publish(Float64(self.q3_left + dqL[2]))
+            self.q1_right_pub.publish(Float64(self.q1_right + dqR[0] - 1.57))
+            self.q2_right_pub.publish(Float64(self.q2_right + dqR[1] - 1.57))
+            self.q3_right_pub.publish(Float64(self.q3_right + dqL[2]))
 
     def payload_pos_cb(self, msg):
         self.dx = msg.x
@@ -104,12 +111,8 @@ class MergeControllerOutputs:
         l3 = self.L3
 
         # Get current position of the right manipulator 
-        x_right_curr = - l1 * cos(q1) \
-                        - l2 * cos(q1 + q2) \
-                        - l3 * cos(q1 + q2 + q3) 
-        y_right_curr = - l1 * sin(q1) \
-                        - l2 * sin(q1 + q2) \
-                        - l3 * sin(q1 + q2 + q3) 
+        x_right_curr = l1 * cos(q1) + l2 * cos(q2 + q1) + l3 * cos(q3 + q2 + q1)
+        y_right_curr = l1 * sin(q1) + l2 * sin(q2 + q1) + l3 * sin(q3 + q2 + q1)
 
         print 'x_right_target: ', x_right_target, ' y_right_target: ', y_right_target
         print 'x_right_curr: ', x_right_curr, ' y_right_curr: ', y_right_curr
@@ -119,23 +122,19 @@ class MergeControllerOutputs:
         dy_right_roll = y_right_target - y_right_curr
 
         # Limit dx / dy
-        #dx_right_pitch = self.saturation(dx_right_pitch, limit)
-        #dy_right_roll = self.saturation(dy_right_roll, limit)
+        dx_right_pitch = self.saturation(dx_right_pitch, limit)
+        dy_right_roll = self.saturation(dy_right_roll, limit)
 
         print 'dx_new: ', dx_right_pitch, ' dy_new: ', dy_right_roll
 
         # New angle increments (right)
         try: 
-            dq1R =  ( (l3 * cos(q1 + q2 + q3) + l2 * cos(q1 + q2)) * dx_right_pitch + \
-                (l3 * sin(q1 + q2 + q3) + l2 * sin(q1 + q2)) * dy_right_roll ) / \
-                (l1 * l3 * sin(q2 + q3) + l1 * l2 * sin(q2)) 
-            dq2R = ( (l3 * cos(q1 + q2 + q3) + l2 * cos(q1 + q2) + l1 * cos(q1)) * dx_right_pitch +  \
-                (l3 * sin(q1 + q2 + q3) + l2 * sin(q1 + q2) + l1 * sin(q1)) * dy_right_roll ) /  \
-                (l1 * l3 * sin(q2 + q3) + l1 * l2 * sin(q2))
+            (dq1R, dq2R, dq3R) = self.func(self.L1, self.L2, self.L3, q1, q2, q3, dx_right_pitch, dy_right_roll)
         except ZeroDivisionError:
             print "ZeroDivisionError"
             dq1R = 0
             dq2R = 0
+            dq3R = 0
         
         print 'dq1R: ', dq1R, ' dq2R: ', dq2R
         print ''
@@ -163,14 +162,10 @@ class MergeControllerOutputs:
 
         print ''
         # Get current position of the right manipulator 
-        x_left_curr = - l1 * cos(q1) \
-                        - l2 * cos(q1 + q2) \
-                        - l3 * cos(q1 + q2 + q3) 
-        y_left_curr = - l1 * sin(q1) \
-                        - l2 * sin(q1 + q2) \
-                        - l3 * sin(q1 + q2 + q3) 
+        x_left_curr = l1 * cos(q1) + l2 * cos(q2 + q1) + l3 * cos(q3 + q2 + q1)
+        y_left_curr = l1 * sin(q1) + l2 * sin(q2 + q1) + l3 * sin(q3 + q2 + q1)
 
-        print 'x_left_target: ', x_left_curr, ' y_left_target: ', y_left_target
+        print 'x_left_target: ', x_left_target, ' y_left_target: ', y_left_target
         print 'x_left_curr: ', x_left_curr, ' y_left_curr: ', y_left_curr
         print 'dx: ', dx_pitch, ' dy: ', dy_roll
 
@@ -179,19 +174,14 @@ class MergeControllerOutputs:
         dy_left_roll = y_left_target - y_left_curr
 
         # Limit dx / dy
-        #dx_left_pitch = self.saturation(dx_left_pitch, limit)
-        #dy_left_roll = self.saturation(dy_left_roll, limit)
+        dx_left_pitch = self.saturation(dx_left_pitch, limit)
+        dy_left_roll = self.saturation(dy_left_roll, limit)
 
         print 'dx_new: ', dx_left_pitch, ' dy_new: ', dy_left_roll
 
         # New angle increments (left)
         try:
-            dq1L =  ( (l3 * cos(q1 + q2 + q3) + l2 * cos(q1 + q2)) * dx_left_pitch + \
-                (l3 * sin(q1 + q2 + q3) + l2 * sin(q1 + q2)) * dy_left_roll ) / \
-                (l1 * l3 * sin(q2 + q3) + l1 * l2 * sin(q2)) 
-            dq2L = ( (l3 * cos(q1 + q2 + q3) + l2 * cos(q1 + q2) + l1 * cos(q1)) * dx_left_pitch +  \
-                (l3 * sin(q1 + q2 + q3) + l2 * sin(q1 + q2) + l1 * sin(q1)) * dy_left_roll ) /  \
-                (l1 * l3 * sin(q2 + q3) + l1 * l2 * sin(q2))
+            (dq1L, dq2L, dq3L) = self.func(self.L1, self.L2, self.L3, q1, q2, q3, dx_left_pitch, dy_left_roll)
         except ZeroDivisionError:
             print "ZeroDivisionError"
             # TODO Handle zero division
@@ -199,25 +189,57 @@ class MergeControllerOutputs:
             #dq2r - any value
             dq2L = 0
             dq1L = 0
+            dq3L = 0
 
         print 'dq1L: ', dq1L, ' dq2L: ', dq2L
         print ''
         
-        return (dq1L, dq2L) 
+        return (dq1L, dq2L, dq3L) 
 
 
+    def func(self, L_C1, L_C2, L_C3, q1, q2, q3, dx, dy):
+        dq1 = (L_C2*L_C3**2*sin(q1 + q2) + L_C1*L_C2**2*sin(q1) + 2*L_C1*L_C3**2*sin(q1) - 2*L_C1*L_C3**2*sin(q1 + 2*q2 + 2*q3) - L_C1*L_C2**2*sin(q1 + 2*q2) \
+                - L_C2*L_C3**2*sin(q1 + q2 + 2*q3) + L_C1*L_C2*L_C3*sin(q1 - q3) - 2*L_C1*L_C2*L_C3*sin(q1 + 2*q2 + q3) + L_C1*L_C2*L_C3*sin(q1 + q3)) * dx / \
+                (L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) + \
+                2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3)) \
+                - (L_C1*L_C2**2*cos(q1) + 2*L_C1*L_C3**2*cos(q1) - 2*L_C1*L_C3**2*cos(q1 + 2*q2 + 2*q3) - L_C1*L_C2**2*cos(q1 + 2*q2) - L_C2*L_C3**2*cos(q1 + q2 + 2*q3) \
+                + L_C2*L_C3**2*cos(q1 + q2) + L_C1*L_C2*L_C3*cos(q1 - q3) - 2*L_C1*L_C2*L_C3*cos(q1 + 2*q2 + q3) + L_C1*L_C2*L_C3*cos(q1 + q3)) * dy / \
+                (L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) \
+                + 2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3) )
+
+        dq2 = (L_C1**2*L_C2*sin(q1 + q2) + L_C2*L_C3**2*sin(q1 + q2) - L_C1*L_C2**2*sin(q1) - L_C1*L_C3**2*sin(q1) + L_C1*L_C3**2*sin(q1 + 2*q2 + 2*q3) -  \
+                L_C1**2*L_C2*sin(q1 - q2) + L_C1*L_C2**2*sin(q1 + 2*q2) + L_C1**2*L_C3*sin(q1 + q2 + q3) + L_C1**2*L_C3*sin(q2 - q1 + q3) - L_C2*L_C3**2*sin(q1 + q2 + 2*q3) \
+                - L_C1*L_C2*L_C3*sin(q1 - q3) + 2*L_C1*L_C2*L_C3*sin(q1 + 2*q2 + q3) - L_C1*L_C2*L_C3*sin(q1 + q3)) * dx/ \
+                (L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) \
+                + 2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3)) \
+                + (L_C1*L_C2**2*cos(q1) + L_C1*L_C3**2*cos(q1) - L_C1*L_C3**2*cos(q1 + 2*q2 + 2*q3) + L_C1**2*L_C2*cos(q1 - q2) - L_C1*L_C2**2*cos(q1 + 2*q2) \
+                - L_C1**2*L_C3*cos(q1 + q2 + q3) + L_C1**2*L_C3*cos(q2 - q1 + q3) + L_C2*L_C3**2*cos(q1 + q2 + 2*q3) - L_C1**2*L_C2*cos(q1 + q2) \
+                - L_C2*L_C3**2*cos(q1 + q2) + L_C1*L_C2*L_C3*cos(q1 - q3) - 2*L_C1*L_C2*L_C3*cos(q1 + 2*q2 + q3) + L_C1*L_C2*L_C3*cos(q1 + q3)) * dy / \
+                (L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) + \
+                2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3))
+
+        dq3 = (L_C3*(L_C1**2*sin(q1 + q2 + q3) + 2*L_C2**2*sin(q1 + q2 + q3) + L_C1**2*sin(q2 - q1 + q3) - 2*L_C2**2*sin(q1 + q2 - q3) - 2*L_C1*L_C2*sin(q1 - q3) + L_C1*L_C2*sin(q1 + 2*q2 + q3) \
+                + 2*L_C2*L_C3*sin(q1 + q2 + 2*q3) + L_C1*L_C2*sin(q1 + q3) - 2*L_C2*L_C3*sin(q1 + q2) - L_C1*L_C3*sin(q1) + L_C1*L_C3*sin(q1 + 2*q2 + 2*q3))) * dx \
+                /(L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) \
+                + 2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3)) \
+                -(L_C3*(L_C1**2*cos(q1 + q2 + q3) + 2*L_C2**2*cos(q1 + q2 + q3) - L_C1**2*cos(q2 - q1 + q3) - 2*L_C2**2*cos(q1 + q2 - q3) + L_C1*L_C2*cos(q1 + 2*q2 + q3) + 2*L_C2*L_C3*cos(q1 + q2 + 2*q3) \
+                + L_C1*L_C2*cos(q1 + q3) - 2*L_C2*L_C3*cos(q1 + q2) - L_C1*L_C3*cos(q1) + L_C1*L_C3*cos(q1 + 2*q2 + 2*q3) - 2*L_C1*L_C2*cos(q1 - q3))) * dy / \
+                (L_C1**2*L_C2**2 + 2*L_C1**2*L_C3**2 + 2*L_C2**2*L_C3**2 - 2*L_C1**2*L_C3**2*cos(2*q2 + 2*q3) - L_C1**2*L_C2**2*cos(2*q2) - 2*L_C2**2*L_C3**2*cos(2*q3) \
+                + 2*L_C1*L_C2*L_C3**2*cos(q2) + 2*L_C1**2*L_C2*L_C3*cos(q3) - 2*L_C1*L_C2*L_C3**2*cos(q2 + 2*q3) - 2*L_C1**2*L_C2*L_C3*cos(2*q2 + q3))
+
+        return (dq1, dq2, dq3)
 
     def joint1_left_cb(self, msg):
-        self.q1_left = msg.process_value
+        self.q1_left = msg.process_value - 1.57
     def joint2_left_cb(self, msg):
-        self.q2_left = msg.process_value
+        self.q2_left = msg.process_value + 1.57
     def joint3_left_cb(self, msg):
         self.q3_left = msg.process_value
     
     def joint1_right_cb(self, msg):
-        self.q1_right = msg.process_value
+        self.q1_right = msg.process_value + 1.57
     def joint2_right_cb(self, msg):
-        self.q2_right = msg.process_value
+        self.q2_right = msg.process_value + 1.57
     def joint3_right_cb(self, msg):
         self.q3_right = msg.process_value
 
